122b42875a38e0307ff5850d70d5b6d509e0be76
Porky-The-Chop <52469386+Porky-The-Chop@users.noreply.github.com>
2019-07-17 13:19:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ document.getElementById("dropbtn").addEventListener("mouseover", dropDownFunc);
 
 function dropDownFunc() {
    document.getElementById("dropdowncontent").classList.toggle("dropdown-content2");
+   
+   if(document.getElementById("dropdowncontent").style.visibility = "hidden") {
+      document.getElementById("dropdowncontent").style.visibility = "visible";
+   } else {
+      document.getElementById("dropdowncontent").style.visibility = "hidden";
+   }
 }
