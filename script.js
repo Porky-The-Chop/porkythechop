99cc1536d7d504d5e58e67390cf998758e1d11ad
Porky-The-Chop <52469386+Porky-The-Chop@users.noreply.github.com>
2019-07-17 12:51:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 document.getElementById("dropbtn").addEventListener("mouseover", dropDownFunc);
 
+var dropDownNum = 0;
+
 function dropDownFunc() {
-   document.getElementById("dropbtn").innerHTML = "hi";
+   if(dropDownNum == 0) {
+      document.getElementById("dropdown").innerHTML = "<button id="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="index.html">About</a>
+    <a href="index1.html">Food</a>
+    <a href="index2.html">Health</a>
+  </div>";
+      dropDownNum = 1;
+   } else {
+      document.getElementById("dropdown").innerHTML = "<button id="dropbtn">Dropdown</button>";
+      dropDownNum = 0;
+   }
 }
