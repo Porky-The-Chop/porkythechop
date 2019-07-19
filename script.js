28112f8a638f28512b54a9c11683d6afae44a756
Porky-The-Chop <52469386+Porky-The-Chop@users.noreply.github.com>
2019-07-18 19:23:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-/*document.getElementById("dropbtn").addEventListener("mouseover", dropDownFunc);
-
-function dropDownFunc() {
-   if(document.getElementById("dropdowncontent").style.visibility = "hidden") {
-      document.getElementById("dropdowncontent").style.visibility = "visible";
-   } else if(document.getElementById("dropdowncontent").style.visibility = "visible") {
-      document.getElementById("dropdowncontent").style.visibility = "hidden";
-   }
-} */
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
