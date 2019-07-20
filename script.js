2209ba5441a9e4897109d4ad0da83f23cf71a059
Porky-The-Chop <52469386+Porky-The-Chop@users.noreply.github.com>
2019-07-19 21:38:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("headerWrapper").innerHTML  = '<div class="dropdown">
+/*document.getElementById("headerWrapper").innerHTML  = '<div class="dropdown">
       <button onclick="myFunction()" class="dropbtn"> <i class="fas fa-bars" style="font-size:18px"></i> </button>
       <div id="myDropdown" class="dropdown-content">
         <a href="index.html">About</a>
@@ -56,7 +56,7 @@ function mySearchFunction() {
     }
   
     document.getElementById("myDropdown1").classList.toggle("show");
-</script>';
+</script>';*/
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
