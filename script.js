4d747ea3a194175c2a8a7fa234222f3b20b77804
Porky-The-Chop <52469386+Porky-The-Chop@users.noreply.github.com>
2019-07-19 21:29:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementByTagId("headerWrapper").innerHTML  = '<div class="dropdown">
+document.getElementById("headerWrapper").innerHTML  = '<div class="dropdown">
       <button onclick="myFunction()" class="dropbtn"> <i class="fas fa-bars" style="font-size:18px"></i> </button>
       <div id="myDropdown" class="dropdown-content">
         <a href="index.html">About</a>
