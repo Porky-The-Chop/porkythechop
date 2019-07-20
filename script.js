8549d3e543a402323c763855d5bfffb3bf6c84db
Porky-The-Chop <52469386+Porky-The-Chop@users.noreply.github.com>
2019-07-19 20:51:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,44 @@ document.getElementByTagId("headerWrapper")[0].innerHTML = "Hello World!"; = "<d
          <li><a href="index.html">About</a></li>
        </ul>
       </div>
-    <h1 id='logo'>PorkyTheChop</h1>";
+    <h1 id='logo'>PorkyTheChop</h1>
+    
+<script>
+        function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function mySearchFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+  
+    document.getElementById("myDropdown1").classList.toggle("show");
+</script>";
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
